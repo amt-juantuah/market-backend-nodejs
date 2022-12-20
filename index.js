81ca8ecec363f2083349529b3eba35d5caeb73e5
mrbridge-dev <oseijuantuah@gmail.com>
2022-12-20 08:47:08 +0000

    set up node and connect to db

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Megatron is going Live Soon");
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//configure dotenv to use and retrieve needs
+dotenv.config();
+const uriString = process.env.MONGO_URL1 + process.env.MONGO_PASS + process.env.MONGO_URL2;
+const listenPort = process.env.PORT;
+
+// set mongoose strictquering to true
+mongoose.set('strictQuery', true);
+
+// connect to db
+mongoose.connect(uriString)
+        .then(() => console.log("Connection Mongoose Established"))
+        .catch((err) => console.log(uriString));
+
+// listen for changes on port
+app.listen( process.env.PORT || 5000, ()=>{
+    console.log("Megatron Backend is Running Live");
+})
